1c4b37932404944f6a711a17ae4b01b356f5a772
jonivorlita <jonlita@dnamicro.com>
2023-04-20 14:20:15 +0800

    update version 3.2.5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Home from './pages/Home';
 import About from './pages/About';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './pages/NavBar';
 import Gallery from './pages/Gallery';
-
+import Splashscreen from './pages/Splashscreen';
 
 function App() {
+  const [showSplashscreen, setShowSplashscreen] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowSplashscreen(false);
+    }, 2500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
-  <>
+  <div className="app">
+  {showSplashscreen && (
+    <div><Splashscreen /></div>
+  )}
   <NavBar/>
     <Routes>
       <Route path="/" element={<Home />} />
@@ -16,7 +32,7 @@ function App() {
       <Route path="/gallery" element={<Gallery/>}/>
     </Routes>
   
-  </>
+  </div>
   );
 }
 
---
+++ src/pages/Splashscreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../style/splashscreen.css";
+
+const SplashScreen = () => {
+
+
+    return (
+        <div className="splash-screen">
+            <div className="loader">
+                <div className="waves"></div>
+            </div>
+            <div className="spinner"></div>
+            <div>
+                <p className='sp'>FLOW <span>MATIC</span></p>
+            </div>
+        </div>
+
+    )
+}
+
+export default SplashScreen;
