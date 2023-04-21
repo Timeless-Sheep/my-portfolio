e7bf523bb193fdf8b2e8b125047e70236d18858c
jonivorlita <jonlita@dnamicro.com>
2023-04-21 14:28:17 +0800

    update version 3.3.1

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setShowSplashscreen(false);
-    }, 2500);
+    }, 3500);
 
     return () => {
       clearTimeout(timeoutId);
---
+++ src/components/About_musician.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class About_musician extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default About_musician
